fc43cf25ffdf11db61d3db0bbcf370d85805694f
Quinn Levine <Qlevine@uci.edu>
2017-02-28 23:38:37 -0800

    Removed extra cameras and made a script to only activate players camera.

---
+++ towerdefense/Assets/Scripts/CamScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CamScript : NetworkBehaviour {
+
+
+    public Camera cam;
+    // Use this for initialization
+    void Start() {
+
+        if (!isLocalPlayer)
+        {
+            cam.enabled = false;
+            return;
+        }
+    }
+
+
+
+    void Update()
+    {
+
+    }
+}
