e99648839bd5d564033bbc4b43d00dde4c82bcaa
Quinn Levine <Qlevine@uci.edu>
2017-02-28 23:04:19 -0800

    Started creating network objects.

---
+++ towerdefense/Assets/CamFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour {
+
+    private GameObject player;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Castle");
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z - 10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
